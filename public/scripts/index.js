876372e4bc99922b91b74def63eba58b27ee5074
manuel-25 <manuelotamendi97@gmail.com>
2023-11-03 19:32:15 -0300

    Category boxes & banner completed

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,17 +1,34 @@
-const radios = document.querySelectorAll('input[name="radio-btn"]')
+window.onload = function() {
+    const radios = document.querySelectorAll('input[name="radio-btn"]')
+    const labels = document.querySelectorAll('.manual-btn')
+    radios[0].checked = true
 
-function changeRadio() {
-    for (let i = 0; i < radios.length; i++) {
-        if (radios[i].checked) {
-            radios[i].checked = false;
-            if (i + 1 < radios.length) {
-                radios[i + 1].checked = true;
-            } else {
-                radios[0].checked = true;
+    let intervalId = setInterval(changeRadio, 6000)
+
+    function changeRadio() {
+        for (let i = 0; i < radios.length; i++) {
+            if (radios[i].checked) {
+                radios[i].checked = false;
+                labels[i].style.background = 'transparent'
+                if (i + 1 < radios.length) {
+                    radios[i + 1].checked = true;
+                    labels[i + 1].style.background = 'var(--white-color)'
+                } else {
+                    radios[0].checked = true;
+                    labels[0].style.background = 'var(--white-color)'
+                }
+                break
             }
-            break;
         }
     }
-}
 
-setInterval(changeRadio, 4000)
+    radios.forEach(function(radio, index) {
+        radio.addEventListener('click', function() {
+            clearInterval(intervalId)
+            intervalId = setInterval(changeRadio, 6000)
+            labels.forEach((label, i) => {
+                label.style.background = i === index ? 'var(--white-color)' : 'transparent'
+            })
+        })
+    })
+}
