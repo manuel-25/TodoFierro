e22b888663a093f2b3631794d45890ab3f2154d1
Manuel-25 <manuelotamendi97@gmail.com>
2023-12-19 16:54:33 -0300

    Removed garbage & MongoDB logs

--- src/config/MongoSingleton.js
+++ src/config/MongoSingleton.js
@@ -1,19 +1,35 @@
-import mongoose from "mongoose"
-import config from './config.js'
+import mongoose from "mongoose";
+import config from './config.js';
 
 class MongoSingleton {
-    static #instance
+    static #instance;
+
     constructor() {
-        mongoose.connect(config.MONGO_URL)
+        mongoose.connect(config.MONGO_URL);
+
+        // Manejar el evento de conexión exitosa
+        mongoose.connection.on('connected', () => {
+            console.log('Conectado a la base de datos MongoDB');
+        });
+
+        // Manejar el evento de error de conexión
+        mongoose.connection.on('error', (err) => {
+            console.error('Error de conexión a la base de datos MongoDB:', err);
+        });
+
+        // Manejar el evento de desconexión
+        mongoose.connection.on('disconnected', () => {
+            console.log('Desconectado de la base de datos MongoDB');
+        });
     }
 
     static getInstance() {
-        if(this.#instance) {
+        if (this.#instance) {
             return this.#instance;
         } else {
-            this.#instance = new MongoSingleton()
+            this.#instance = new MongoSingleton();
         }
     }
 }
 
-export default MongoSingleton
+export default MongoSingleton;
