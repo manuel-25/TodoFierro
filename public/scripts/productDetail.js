056f70e050a6b77be1d8479490fe6f3bb4b12481
Manuel-25 <manuelotamendi97@gmail.com>
2023-12-19 18:24:32 -0300

    Socials updated & Whatssap consultar

--- public/scripts/productDetail.js
+++ public/scripts/productDetail.js
@@ -1,12 +1,36 @@
-const mainThumbnail = document.getElementById('main-thumbnail')
-const thumbnailImages = document.querySelectorAll('.thumbnails img')
+document.addEventListener('DOMContentLoaded', () => {
+    const mainThumbnail = document.getElementById('main-thumbnail');
+    const thumbnailImages = document.querySelectorAll('.thumbnails img');
+    const buyButton = document.getElementById('buyButton');
+    const productName = document.getElementById('product-name').innerHTML
 
-thumbnailImages.forEach(thumbnail => {
-    thumbnail.addEventListener('click', () => {
-        mainThumbnail.src = thumbnail.src
-        thumbnailImages.forEach(image => {
-            image.classList.remove('selectedThumbnail')
-        })
-        thumbnail.classList.add('selectedThumbnail')
+    //productID
+    const currentURL = window.location.href;
+    const url = new URL(currentURL);
+    const productID = url.pathname.split('/').pop()
+
+    buyButton.addEventListener('click', () => {
+        const selectedColor = document.getElementById('colorSelect').value;
+        const selectedMaterial = document.getElementById('materialSelect').value;
+        const quantity = document.getElementById('quantityInput').value;
+
+        const message = `¡Hola! Me interesa comprar el producto ${productName} (${productID}), color ${selectedColor}, material ${selectedMaterial}, cantidad ${quantity}. ¿Puedes proporcionarme más información?`;
+
+        const encodedMessage = encodeURIComponent(message);
+        const whatsappURL = `https://wa.me/5491158338131?text=${encodedMessage}`;
+
+        // Redirige al usuario a la URL de WhatsApp
+        window.location.href = whatsappURL;
     })
-})
+
+    // Thumbnails selector
+    thumbnailImages.forEach(thumbnail => {
+        thumbnail.addEventListener('click', () => {
+            mainThumbnail.src = thumbnail.src;
+            thumbnailImages.forEach(image => {
+                image.classList.remove('selectedThumbnail');
+            });
+            thumbnail.classList.add('selectedThumbnail');
+        });
+    });
+});
