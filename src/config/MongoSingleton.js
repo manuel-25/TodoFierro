4128fad6679b80b2ea60dc916ea854113ac68b14
Manuel-25 <manuelotamendi97@gmail.com>
2023-12-19 17:30:54 -0300

    detail product.id (_id) bug fixed

--- src/config/MongoSingleton.js
+++ src/config/MongoSingleton.js
@@ -7,20 +7,17 @@ class MongoSingleton {
     constructor() {
         mongoose.connect(config.MONGO_URL);
 
-        // Manejar el evento de conexión exitosa
         mongoose.connection.on('connected', () => {
-            console.log('Conectado a la base de datos MongoDB');
-        });
+            console.log('Conectado a la base de datos MongoDB')
+        })
 
-        // Manejar el evento de error de conexión
         mongoose.connection.on('error', (err) => {
-            console.error('Error de conexión a la base de datos MongoDB:', err);
-        });
+            console.error('Error de conexión a la base de datos MongoDB:', err)
+        })
 
-        // Manejar el evento de desconexión
         mongoose.connection.on('disconnected', () => {
-            console.log('Desconectado de la base de datos MongoDB');
-        });
+            console.log('Desconectado de la base de datos MongoDB')
+        })
     }
 
     static getInstance() {
